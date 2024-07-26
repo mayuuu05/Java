d8313c29d70372744930348a074633ead9d3ac54
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-07-26 21:03:44 +0530

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,20 @@
+  class Main{
+
+    public static void main(String arg[])
+    {
+        System.out.println("Hello Java Program");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+         int f = a / b;
+         int g = a % b;
+
+        System.out.println("Addition of "+ a + " + " +  b + " is = "  + c);
+        System.out.println("Subtraction of "+ a + " - " +  b + " is = "  + d);
+        System.out.println("Multiplication of "+ a + " * " +  b + " is = "  + e);
+        System.out.println("Division of "+ a + " / " +  b + " is = "  + f);
+        System.out.println("Reminder of "+ a + " % " +  b + " is = "  + g);
+    }
+  }
